a4bc4fe2ad67ae1bbe89c4bda879b01499eb6146
Yosi Golan <yogolan@microsoft.com>
2020-08-09 15:50:34 +0300

    Insert project

---
+++ ServiceFabricLongProcessingWIthDI/StatelessNativeService/InjectedService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatelessNativeService
+{
+    public class InjectedService
+    {
+        public void DoSomthing()
+        {
+            Trace.TraceInformation("InjectedService I did something");
+        }
+    }
+}
